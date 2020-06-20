c4395e9d026094416b2640221113347680cbfe6e
cai <flashmx29403229@gmail.com>
2020-06-20 16:37:12 +0800

    更新

--- demo3/index.js
+++ demo3/index.js
@@ -4,35 +4,35 @@ import { Spark } from "./spark.js";
 import { Electricity } from "./electricity.js";
 function getTextData(s, font) {
   if (s.length <= 0) return;
-  let pool = document.createElement("canvas");
-  let buffer = pool.getContext("2d");
+  const pool = document.createElement("canvas");
+  const buffer = pool.getContext("2d");
   buffer.font = font;
-  let measureText = buffer.measureText(s);
-  let tw = measureText.width;
-  let th = measureText.actualBoundingBoxAscent + measureText.actualBoundingBoxDescent;
+  const measureText = buffer.measureText(s);
+  const tw = measureText.width;
+  const th = measureText.actualBoundingBoxAscent + measureText.actualBoundingBoxDescent;
   pool.width = tw;
   pool.height = th + 2;
   buffer.font = font;
   buffer.fillStyle = "#ffffff";
   buffer.fillText(s, 0, th);
-  let imageData = buffer.getImageData(0, 0, pool.width, pool.height);
-  let data = imageData.data;
+  const imageData = buffer.getImageData(0, 0, pool.width, pool.height);
+  const data = imageData.data;
   function getIndex(imageData, x, y) {
     return (x + y * imageData.width) * 4;
   }
-  let dir = [
+  const dir = [
     [0, -1],
     [1, 0],
     [0, 1],
     [-1, 0],
   ];
-  let posList = [];
-  let temp = ctx.createImageData(imageData.width, imageData.height);
+  const posList = [];
+  const temp = ctx.createImageData(imageData.width, imageData.height);
   for (let j = 0; j < imageData.height; j++) {
     for (let i = 0; i < imageData.width; i++) {
-      let index = getIndex(imageData, i, j);
+      const index = getIndex(imageData, i, j);
       if (data[index + 3] > 127) {
-        let bool = dir.some((el) => {
+        const bool = dir.some((el) => {
           if (i + el[0] < 0 || i + el[0] >= imageData.width || j + el[1] < 0 || j + el[1] >= imageData.height) {
             return true;
           }
@@ -49,13 +49,13 @@ function getTextData(s, font) {
     }
   }
 
-  let spliceList = [];
+  const spliceList = [];
   let tempPos;
   for (let i = 0; i < posList.length - 1; i++) {
     let index = -1;
     for (let j = i + 1; j < posList.length; j++) {
-      let x = Math.abs(posList[i][0] - posList[j][0]);
-      let y = Math.abs(posList[i][1] - posList[j][1]);
+      const x = Math.abs(posList[i][0] - posList[j][0]);
+      const y = Math.abs(posList[i][1] - posList[j][1]);
       if (x <= 1 && y <= 1) {
         index = j;
         if (x * y === 0) {
@@ -73,7 +73,7 @@ function getTextData(s, font) {
   }
   spliceList.push(posList.length);
 
-  let posListG = [];
+  const posListG = [];
   let index = 0;
   for (let i = 0; i < spliceList.length; i++) {
     posListG.push(posList.slice(index, spliceList[i]));
@@ -89,12 +89,12 @@ function getTextData(s, font) {
   return { width: imageData.width, height: imageData.height, posList, posListG };
 }
 
-function getNearestDistance(pos, posList) {
-  let data = { i: -1, j: -1, r: Infinity };
-  for (let i = 0; i < text_posListG.length; i++) {
-    let posList = text_posListG[i];
+function getNearestDistance(pos, posListG) {
+  const data = { i: -1, j: -1, r: Infinity };
+  for (let i = 0; i < posListG.length; i++) {
+    const posList = posListG[i];
     for (let j = 0; j < posList.length; j++) {
-      let r = getDistance(posList[j], pos);
+      const r = getDistance(posList[j], pos);
       if (r < data.r) {
         data.i = i;
         data.j = j;
@@ -106,7 +106,7 @@ function getNearestDistance(pos, posList) {
 }
 
 function drawLight(ctx, pp, rr) {
-  let gradient = ctx.createRadialGradient(pp[0], pp[1], 2, pp[0], pp[1], rr);
+  const gradient = ctx.createRadialGradient(pp[0], pp[1], 2, pp[0], pp[1], rr);
   gradient.addColorStop(0, "hsla(0,100%,100%,0.5)");
   gradient.addColorStop(0.15, "hsla(90,100%,75%,0.25)");
   gradient.addColorStop(1, "hsla(180,100%,50%,0)");
@@ -118,7 +118,7 @@ function drawLight(ctx, pp, rr) {
 }
 
 function drawE(ctx, electricity, pp0, pp1, r, lineWidth, glow, light0, light1) {
-  let m = r / electricity.segmentNum;
+  const m = r / electricity.segmentNum;
   electricity.data_deform = m;
   electricity.data_wave = electricity.data_limit * m;
   electricity.update();
@@ -146,39 +146,38 @@ function shake(value, swing, limit) {
   return numberCrop(value + swing * (1 - Math.random() * 2), -limit, limit);
 }
 
-let canvas, ctx, cWidth, cHeight;
-canvas = document.getElementById("canvas");
-ctx = canvas.getContext("2d");
-cWidth = canvas.width;
-cHeight = canvas.height;
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const cWidth = canvas.width;
+const cHeight = canvas.height;
 
-let mPos = [0, 0];
+const mPos = [0, 0];
 
 canvas.addEventListener("mousemove", function (el) {
   mPos[0] = el.offsetX;
   mPos[1] = el.offsetY;
 });
-let text = "POWER";
-let textFont = "bold 100px Courier New";
-let textData = getTextData(text, textFont);
-let textPos = [cWidth * 0.5 - textData.width * 0.5, cHeight * 0.5 - textData.height * 0.5];
+const text = "POWER";
+const textFont = "bold 100px Courier New";
+const textData = getTextData(text, textFont);
+const textPos = [cWidth * 0.5 - textData.width * 0.5, cHeight * 0.5 - textData.height * 0.5];
 
-let text_posListG = textData.posListG;
+const text_posListG = textData.posListG;
 
-let text_data = [];
+const text_data = [];
 
 for (let i = 0; i < 10; i++) {
-  let electricity = new Electricity(20);
+  const electricity = new Electricity(20);
   electricity.setSegmentAngleParameter();
   text_data.push({ id: -1, electricity: electricity, indexPos: [], transform: [], reset: true });
 }
 
 let main_transform = 0;
-let main_electricity = new Electricity(50, 0.05, 0.15);
+const main_electricity = new Electricity(50, 0.05, 0.15);
 
-let sub_data = [];
+const sub_data = [];
 for (let i = 0; i < 2; i++) {
-  let sub_electricity = new Electricity(20);
+  const sub_electricity = new Electricity(20);
   //sub_electricity.setSegmentAngleParameter(0.05, 0.15);
   //sub_electricity.setSegmentPosParameter(1, 20, 20);
   sub_data.push({ electricity: sub_electricity, indexPos: [], transform: [], reset: true });
@@ -197,21 +196,21 @@ function update() {
   //setShadow(0, 0, 2 * 2, "#00ffff");
   //ctx.drawImage(text.image, 50, 200);
 
-  let temp_mPos = getVector(textPos, mPos);
+  const temp_mPos = getVector(textPos, mPos);
   ctx.save();
   ctx.translate(textPos[0], textPos[1]);
   ctx.globalCompositeOperation = "lighter";
   ctx.lineJoin = "round";
   ctx.lineCap = "round";
 
-  let indexData = getNearestDistance(temp_mPos, text_posListG);
-  let dischargeRange = 200;
-  let discharge = indexData.r < dischargeRange;
+  const indexData = getNearestDistance(temp_mPos, text_posListG);
+  const dischargeRange = 200;
+  const discharge = indexData.r < dischargeRange;
 
   setShadow(ctx, 0, 0, 2 * 5, "#00ffff");
   ctx.font = textFont;
-  let measureText = ctx.measureText(text);
-  let th = measureText.actualBoundingBoxAscent + measureText.actualBoundingBoxDescent;
+  const measureText = ctx.measureText(text);
+  const th = measureText.actualBoundingBoxAscent + measureText.actualBoundingBoxDescent;
   ctx.fillStyle = discharge ? "#ffffffee" : "#ffffffcc";
   ctx.fillText(text, 0, th);
   /*ctx.lineWidth = 3;
@@ -220,15 +219,15 @@ function update() {
 
   for (let i = 0; i < (discharge ? text_data.length : 3); i++) {
     let id = text_data[i].id;
-    let electricity = text_data[i].electricity;
-    let indexPos = text_data[i].indexPos;
-    let transform = text_data[i].transform;
+    const electricity = text_data[i].electricity;
+    const indexPos = text_data[i].indexPos;
+    const transform = text_data[i].transform;
     let bool = false;
     if (text_data[i].reset || Math.random() > (discharge ? 0.7 : 0.9)) {
       id = Math.floor(Math.random() * text_posListG.length);
       text_data[i].id = id;
       electricity.init();
-      let startIndex = Math.floor(Math.random() * text_posListG[id].length);
+      const startIndex = Math.floor(Math.random() * text_posListG[id].length);
       indexPos[0] = startIndex;
       indexPos[1] =
         startIndex +
@@ -238,20 +237,20 @@ function update() {
       bool = true;
     }
 
-    let posList = text_posListG[id];
+    const posList = text_posListG[id];
 
-    let index = pushVector(indexPos, transform);
+    const index = pushVector(indexPos, transform);
 
-    let startPos = getArrayCycle(posList, index[0]);
-    let endPos = getArrayCycle(posList, index[1]);
+    const startPos = getArrayCycle(posList, index[0]);
+    const endPos = getArrayCycle(posList, index[1]);
 
-    let r = getDistance(startPos, endPos);
+    const r = getDistance(startPos, endPos);
 
     if (r > 10 && r < 40) {
       text_data[i].reset = false;
       transform[0] = shake(transform[0], 2, 5);
       transform[1] = shake(transform[1], 2, 5);
-      let rand = 1 + Math.random();
+      const rand = 1 + Math.random();
       drawE(ctx, electricity, startPos, endPos, r, rand, rand * 2, rand * 8, rand * 8);
 
       if (bool || Math.random() > 0.95) {
@@ -282,9 +281,9 @@ function update() {
     }
 
     main_transform = shake(main_transform, 2, 10);
-    let pp = getArrayCycle(text_posListG[indexData.i], indexData.j + main_transform);
+    const pp = getArrayCycle(text_posListG[indexData.i], indexData.j + main_transform);
 
-    let main_rate = 0.25 + 0.75 * (1 - indexData.r / dischargeRange);
+    const main_rate = 0.25 + 0.75 * (1 - indexData.r / dischargeRange);
 
     drawE(
       ctx,
@@ -297,7 +296,7 @@ function update() {
       0,
       100 * (main_rate + 0.5 * Math.random())
     );
-    let angle = Math.atan2(pp[1] - temp_mPos[1], pp[0] - temp_mPos[0]);
+    const angle = Math.atan2(pp[1] - temp_mPos[1], pp[0] - temp_mPos[0]);
     if (Math.random() > 0.5) {
       //for (let i = 0; i < 2; i++) {
       particles.push(
@@ -318,12 +317,12 @@ function update() {
       //}
     }
 
-    let posList = main_electricity.segmentShow;
+    const posList = main_electricity.segmentShow;
 
     for (let i = 0; i < sub_data.length; i++) {
-      let electricity = sub_data[i].electricity;
-      let indexPos = sub_data[i].indexPos;
-      let transform = sub_data[i].transform;
+      const electricity = sub_data[i].electricity;
+      const indexPos = sub_data[i].indexPos;
+      const transform = sub_data[i].transform;
 
       if (sub_data[i].reset || Math.random() > 0.9) {
         electricity.init();
@@ -333,15 +332,15 @@ function update() {
         transform[1] = 0;
       }
 
-      let index = pushVector(indexPos, transform);
-      let pp0 = getArrayCycle(posList, index[0]);
-      let pp1 = getArrayCycle(text_posListG[indexData.i], indexData.j + index[1]);
-      let r = getDistance(pp0, pp1);
+      const index = pushVector(indexPos, transform);
+      const pp0 = getArrayCycle(posList, index[0]);
+      const pp1 = getArrayCycle(text_posListG[indexData.i], indexData.j + index[1]);
+      const r = getDistance(pp0, pp1);
       if (r < 50) {
         transform[0] = shake(transform[0], 2, 20);
         transform[1] = shake(transform[1], 2, 20);
         sub_data[i].reset = false;
-        let rate = (0.25 + 0.75 * (1 - r / 50)) * 0.7 + main_rate * 0.3;
+        const rate = (0.25 + 0.75 * (1 - r / 50)) * 0.7 + main_rate * 0.3;
         drawE(ctx, electricity, pp0, pp1, r, 3 * rate, 3 * rate * 5, 0, 100 * (rate + 0.5 * Math.random()));
         if (Math.random() > 0.5) {
           //for (let i = 0; i < 2; i++) {
@@ -379,20 +378,23 @@ function update() {
 
   ctx.restore();
 }
-let particles = [];
+const particles = [];
 update();
 let oldTime = Date.now();
-let count = 0;
 let animate = function () {
   requestAnimationFrame(animate);
   let nowTime = Date.now();
   let delta = (nowTime - oldTime) / 1000;
   oldTime = nowTime;
 
-  count += delta;
-  if (count >= 0.02) {
-    count %= 0.02;
-    update();
-  }
+  update();
+
+  ctx.save();
+  ctx.font = "bold 18px Noto Sans TC";
+  ctx.textAlign = "start";
+  ctx.textBaseline = "hanging";
+  ctx.fillStyle = "#ffffff";
+  ctx.fillText((1 / delta).toFixed(1), 10, 10);
+  ctx.restore();
 };
 animate();
